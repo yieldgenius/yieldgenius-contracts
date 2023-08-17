445a68ef83ef80dc00ac9a8341b3893ea58aa4ab
yieldgenius <yieldgenius@proton.me>
2023-08-18 00:32:46 +0200

    alien swap SC added, with deploy script for cbETH-ETH

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,6 +44,11 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY_ARBITRUM !== undefined ? [process.env.PRIVATE_KEY_ARBITRUM] : [],
       chainId: 42161,
     },
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: process.env.PRIVATE_KEY_ARBITRUM !== undefined ? [process.env.PRIVATE_KEY_ARBITRUM] : [],
+      chainId: 8453,
+    },
   },
   gasReporter: {
     enabled: true,
@@ -51,7 +56,20 @@ const config: HardhatUserConfig = {
     coinmarketcap: process.env.CMC_API,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API,
+    apiKey: {
+      arbitrum: 'AKXXQE6ZZXM8F6F5CS4EJK5DH3M596SK9J',
+      'base-mainnet': 'YY6GP743U9KFKVHZW2T52IC8I8XZ5T9CV7'
+    },
+    customChains: [
+      {
+        network: "base-mainnet",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org/"
+        }
+      }
+    ]
   },
   mocha: {
     timeout: 400000000
